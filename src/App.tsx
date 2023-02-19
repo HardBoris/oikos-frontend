6904ae01e73de3721c55658eb0e7ae832dc9d833
HardBoris <bgaibor03@gmail.com>
2023-02-19 14:50:27 -0400

    feat: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "./styles/global.css";
+import "react-toastify/dist/ReactToastify.css";
+import { AppRouter } from "./routes";
+import { ToastContainer } from "react-toastify";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      }
+      <AppRouter />
+    </>
   );
-}
+};
 
 export default App;
