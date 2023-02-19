f2bd9a23a90a7fe8627ae37768434939d1496d85
HardBoris <bgaibor03@gmail.com>
2023-02-19 15:34:09 -0400

    feat: home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import logo from "../../logo.svg";
+
 export const Home = () => {
   return (
     <div>
@@ -8,6 +10,22 @@ export const Home = () => {
       <p>
         <a href="/signup">Cadastro</a>
       </p>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </div>
   );
 };
