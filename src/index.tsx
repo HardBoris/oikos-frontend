a0e54533857f1189540e1c2977e4d307940304c2
HardBoris <bgaibor03@gmail.com>
2023-02-19 14:49:55 -0400

    feat: index refatored

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { AppProvider } from "./context";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
